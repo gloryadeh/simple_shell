7ea5b35600658cd24809d042004f7d3526503de1
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-06-11 22:04:34 +0100

    make function return and not exit when command is missing in execute.c

--- execute.c
+++ execute.c
@@ -29,7 +29,7 @@ void execute(char **av, char **argv)
 			write(2, argv[0], _strlen(argv[0])), write(2, ": ", 2);
 			write(2, "1: ", 3), write(2, av[0], _strlen(av[0]));
 			write(2, ": not found\n", 12);
-			exit(EXIT_FAILURE);
+			return;
 		}
 	}
 	/* check if we are in parent or child process */
