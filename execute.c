68bf1daf986c949276859888fc57ac1840a12ff7
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-06-15 19:07:19 +0100

    Edit error message

--- execute.c
+++ execute.c
@@ -22,8 +22,7 @@ void execute(char **av, char **argv)
 		if (builtin_stat != 0)
 		{
 			write(2, argv[0], _strlen(argv[0])), write(2, ": ", 2);
-			write(2, "1: ", 3), write(2, av[0], _strlen(av[0]));
-			write(2, ": not found\n", 12);
+			write(2, "No such file or directory\n", 26);
 			return;
 		}
 	}
